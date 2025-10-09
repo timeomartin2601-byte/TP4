e1bc612eb26ec3b09bcd617d2f8fca87e736c649
timeomartin2601-byte <timeomartin2601@gmail.com>
2025-10-09 12:06:44 +0200

    Update TP4_fct.py

--- TP4_fct.py
+++ TP4_fct.py
@@ -5,25 +5,33 @@ mw.title("casse brique")
 mw.geometry('1920x1080')
 
 
-canva=Canvas(mw, bg = 'ivory' ) 
+canva=Canvas(mw, bg = 'snow' ) 
 canva.grid(row = 0, column = 0, ipadx = 1920, ipady = 1080 )
 
 
-l = Label(canva,bg = "red")
+l = Label(canva,bg = "snow")
 l.grid(row = 0, column = 0, ipadx = 100, padx = 0)
 
-l1 = Label(l, text = "Nbr vies : ",bg = "green")
-l1.grid(row = 0, column = 0, ipadx = 100, padx = 0)
+l1 = Label(l, text = "Nbr vies : ",bg = "snow")
+l1.grid(row = 0, column = 0, ipadx = 20, padx = 0)
 
-l2 = Label(l, text = "score : ",bg = "green")
-l2.grid(row = 0, column = 1, ipadx = 100, padx = 0)
+l2 = Label(l, text = "score : ",bg = "snow")
+l2.grid(row = 0, column = 1, ipadx = 20, padx = 0)
 
-boutton = Button( l, text= 'quitter', command= mw.destroy)
-boutton.grid(row = 0, column = 2, ipadx = 100, padx = 200)
+l3 = Label(l,bg = "snow")
+l3.grid(row = 0, column = 2, ipadx = 582, padx = 0)
+
+
+
+boutton2 = Button( l, text= 'restart', command= mw.destroy, bg = "snow").grid(row = 0, column = 3, ipadx = 20)
+boutton1 = Button( l, text= 'quitter', command= mw.destroy, bg = "snow").grid(row = 0, column = 4, ipadx = 20)
+
+
+
 
 for l in range (15) : 
     for c in range (8) : 
-        canva.create_rectangle(20 + ( 100 * l ), 30 + ( 40 * c ), 110 + ( 100 * l ) , 60 + ( 40 * c ), fill="blue")
+        canva.create_rectangle(20 + ( 100 * l ), 40 + ( 40 * c ), 110 + ( 100 * l ) , 70 + ( 40 * c ), fill="dark green")
 
 
 
