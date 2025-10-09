c48fff2f4253834eab44330b481cc5136e57060c
timeomartin2601-byte <timeomartin2601@gmail.com>
2025-10-09 11:06:24 +0200

    Update TP4_fct.py

--- TP4_fct.py
+++ TP4_fct.py
@@ -1,4 +1,4 @@
-from tkinter import Tk,Label,Canvas
+from tkinter import Tk,Label,Canvas,Button
 
 mw=Tk()
 mw.title("casse brique")
@@ -9,13 +9,21 @@ canva=Canvas(mw, bg = 'ivory' )
 canva.grid(row = 0, column = 0, ipadx = 1920, ipady = 1080 )
 
 
-
-l = Label(canva, text = "Nbr vies : ",bg = "red")
+l = Label(canva,bg = "red")
 l.grid(row = 0, column = 0, ipadx = 100, padx = 0)
 
-l2 = Label(canva, text = "Nbr vies : ",bg = "red")
-l2.grid(row = 0, column = 1, ipadx = 100, padx = 1020)
+l1 = Label(l, text = "Nbr vies : ",bg = "green")
+l1.grid(row = 0, column = 0, ipadx = 100, padx = 0)
 
+l2 = Label(l, text = "score : ",bg = "green")
+l2.grid(row = 0, column = 1, ipadx = 100, padx = 0)
+
+boutton = Button( l, text= 'quitter', command= mw.destroy)
+boutton.grid(row = 0, column = 2, ipadx = 100, padx = 200)
+
+for l in range (15) : 
+    for c in range (8) : 
+        canva.create_rectangle(20 + ( 100 * l ), 30 + ( 40 * c ), 110 + ( 100 * l ) , 60 + ( 40 * c ), fill="blue")
 
 
 
