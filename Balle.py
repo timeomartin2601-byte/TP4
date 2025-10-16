b6fb5b880ea44dc0cb3c261a64f56181b0a2374f
Arno Braz <arno.braz.69@gmail.com>
2025-10-16 12:14:01 +0200

    Update Balle.py

--- Balle.py
+++ Balle.py
@@ -10,8 +10,8 @@ from random import randint
 class Balle : 
     def __init__(self, canvas):
         self.__balle = canvas.create_oval(760, 730, 770, 740, fill = "red")
-        self.__vitx = -randint(2, 10)
-        self.__vity = -randint(2, 10)
+        self.__vitx = -10
+        self.__vity = -10
 
     def deplacement(self, canvas):
         '''
@@ -21,9 +21,9 @@ class Balle :
                 - inversion du y à chaque collision (sauf si collision latéral)
         '''
         if self.collision_hori(canvas): # TODO collision -> Bool ? Coord ? les 2 ? Type de collision (latéral ou horizontal)
-            self.__vitx = -self.__vitx
-        if self.collision_hori(canvas): # TODO collision -> Bool ? Coord ? les 2 ? Type de collision (latéral ou horizontal)
             self.__vity = -self.__vity
+        if self.collision_lat(canvas): # TODO collision -> Bool ? Coord ? les 2 ? Type de collision (latéral ou horizontal)
+            self.__vitx = -self.__vitx
 
         canvas.move(self.__balle, self.__vitx, self.__vity) 
 
@@ -32,11 +32,11 @@ class Balle :
         Detecte si il y a collision avec un bloc (ou la raquette) ou avec la bordure y = 0 (si y = 1080 : fin de la partie)
         Si la coord y de la balle est inférieur à y = 350 (blocs le + bas) : on appelle bloc de cassage (comment faire car autre class ?)
         '''
-        return canvas.coords(self.__balle)[1] == 600 or canvas.coords(self.__balle)[3] == 900 
+        return canvas.coords(self.__balle)[1] == 0 or canvas.coords(self.__balle)[3] == 1080 
 
     def collision_lat(self, canvas):
         '''
         Detecte si il y a collision avec un bloc (ou la raquette) ou avec l'une des bordures x = 0 ou x = 1920
         Si la coord y de la balle est inférieur à y = 350 (blocs le + bas) : on appelle bloc de cassage (comment faire car autre class?)
         '''
-        return canvas.coords(self.__balle)[0] == 100
+        return canvas.coords(self.__balle)[0] == 0 or canvas.coords(self.__balle)[2] == 1920
