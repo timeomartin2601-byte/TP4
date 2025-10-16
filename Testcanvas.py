404b191b1d0ba0d41c8c735d2b85bd86cc19b350
timeomartin2601-byte <timeomartin2601@gmail.com>
2025-10-16 12:04:46 +0200

    Update Testcanvas.py

--- Testcanvas.py
+++ Testcanvas.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import Blocs as blc
-#import raquette as pal
+import raquette as pal
 import Balle as bal
 
 try:
@@ -15,6 +15,7 @@ except Exception:
 mw = tk.Tk()
 mw.title("Casse Brique")
 mw.geometry('1920x1080')
+mw.attributes('-fullscreen', True)
 
 # Création du Canvas
 
@@ -56,13 +57,16 @@ mw.bind("<Button-1>", destr)
     
 
     
-# #Creation palet
-# palet=pal.palet(canvas)
+#Creation palet
+palet=pal.palet(canvas)
 
-# def mouv_droite(event):
-#     palet.droite()
+def mouv(event):
+    if event.keysym == "Right":
+        palet.droite(canvas)
+    if event.keysym == "Left":
+        palet.gauche(canvas)
 
-# mw.bind("<Right>", mouv_droite)
+mw.bind("<Key>", mouv)
 
 mw.resizable(False, False)
 
