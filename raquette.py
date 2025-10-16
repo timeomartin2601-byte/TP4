7ee21fe000490caf8eaf9f87392bf236a2e409c5
Arno Braz <arno.braz.69@gmail.com>
2025-10-16 11:53:37 +0200

    Update raquette.py

--- raquette.py
+++ raquette.py
@@ -5,5 +5,17 @@ class palet:
         self.canvas = canvas
         self.palet = canvas.create_rectangle(710, 730, 820, 745, fill="black")
 
+<<<<<<< Updated upstream
     def droite(self,canvas):
         canvas.move(self.palet, 5, 0)
+=======
+class palet : 
+    def __init__(self,canvas):
+        palet=canvas.create_rectangle(710,630,820,645,  fill="black")
+        self.palet=palet
+
+    def droite(canvas) :
+        pass
+
+
+>>>>>>> Stashed changes
