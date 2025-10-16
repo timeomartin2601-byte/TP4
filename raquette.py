32fea69effbcd3ef07313990c02faec44fe4e4d9
timeomartin2601-byte <timeomartin2601@gmail.com>
2025-10-16 09:54:02 +0200

    Update raquette.py

---
+++ raquette.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+class palet : 
+    def __init__(self,canvas):
+        palet=canvas.create_rectangle(710,630,820,645,  fill="black")
+        self.palet=palet
+
+    def droite(canvas) :
+        canvas.mouv(palet,5,0)
+
+
