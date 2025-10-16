752028b77840330e85b031a1a961e5d53af6f2a5
Arno Braz <arno.braz.69@gmail.com>
2025-10-16 09:05:47 +0200

    Update Testcanvas.py

--- Testcanvas.py
+++ Testcanvas.py
@@ -1,37 +1,30 @@
 import tkinter as tk
 import Blocs as blc
 
+# Création de la fenêtre
+
 mw = tk.Tk()
-mw.title("casse brique")
+mw.title("Casse Brique")
 mw.geometry('1920x1080')
+
+# Création du Canvas
 
 canvas = tk.Canvas(mw, bg='ivory')
 canvas.grid(row=0, column=0, ipadx=1920, ipady=1080)
 
 tk.Button(canvas, text="Quitter", command=mw.destroy).grid(row=0, column=2, ipadx=100, padx=750)
 
-l = tk.Label(canvas, text="Nbr vies :", bg="red")
+l = tk.Label(canvas, text="Nbr vies :", bg="red") # TODO Ajouter la var du nbr de vie 
 l.grid(row=0, column=0, ipadx=100, padx=0)
 
-l2 = tk.Label(canvas, text="Nbr vies :", bg="red")
+l2 = tk.Label(canvas, text="Nbr vies :", bg="red") # TODO Ajouter la var du nbr de vie 
 l2.grid(row=0, column=1, ipadx=100, padx=10)
 
-# Création des blocs
-# blocs = dict()
-# for l in range(15):
-#     for c in range(8):
-#         blocs[(20 + (100 * l), 30 + (40 * c))] = canvas.create_rectangle(
-#             20 + (100 * l), 30 + (40 * c),
-#             110 + (100 * l), 60 + (40 * c),
-#             fill="blue"
-#         )
-
-# Création de ton gestionnaire de blocs
+# Création des Blocs
 B = blc.Blocs(canvas)
 
 def on_key(event):
     if event.keysym == "space":
-        # Appelle la méthode cassage de ta classe
         B.cassage(canvas, coord=(220, 30))
 
 def destr(event):
