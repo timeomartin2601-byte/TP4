0118ccaa503ac312c96fc7fd00cf27176c48070c
timeomartin2601-byte <timeomartin2601@gmail.com>
2025-10-16 11:05:19 +0200

    Update raquette.py

--- raquette.py
+++ raquette.py
@@ -4,6 +4,20 @@ class palet:
     def __init__(self, canvas):
         self.canvas = canvas
         self.palet = canvas.create_rectangle(710, 730, 820, 745, fill="black")
+        self.mur_d=820
+        self.mur_g=710
 
     def droite(self,canvas):
-        canvas.move(self.palet, 5, 0)
+        if self.mur_d==canvas.create_rectangle(1810, 730, 1920, 745, fill="black"):
+            canvas.delete(self.palet)
+        else : 
+            canvas.move(self.palet, 10, 0)
+            mur_d+=5
+            mur_g+=5            
+
+
+    def gauche(self,canvas):
+        if self.mur_d>=0:
+            canvas.move(self.palet, 10, 0)
+            self.mur_g-=5
+            self.mur_d-=5
