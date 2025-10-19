118778968c8400c9fdcd474a3c27e316ebd917e5
Arno Braz <arno.braz.69@gmail.com>
2025-10-20 00:01:25 +0200

    fix : les dimensions des frames et le déroulement du jeu

--- Casse_Brique.py
+++ Casse_Brique.py
@@ -2,9 +2,9 @@
 Casse-Brique : Programme principal
 Braz Arno, Martin Timeo
 16/10/25
-TODO :  - assembler les différentes classes ensemble
-        - ajouter des paramètres de jeu
-        - idéalement remplacer un max de forme par des images pour que ce soit plus beau et agréable à jouer
+TODO :  - Gestion des vies
+        - Prévoir un bouton rejouer à la fin
+        - Idéalement remplacer un max de forme par des images pour que ce soit plus beau et agréable à jouer
 '''
 
 # Importation des modules et des classes
@@ -23,24 +23,23 @@ window.geometry('1920x1080')
 window.attributes('-fullscreen', True)
 
 frame_info = tk.Frame(window, width=1920, height=20, bg='grey')
-frame_info.pack(fill='both', expand=True)
+frame_info.pack(fill='both')
 
 btn_close = tk.Button(frame_info, text="X", command=window.destroy)
 btn_close.pack(side='right')
 
-# TODO Création d'un menu (button etc)
+# Création du menu
 
 frame_menu = tk.Frame(window, width=1920, height=1060)
 frame_menu.pack(fill='both', expand=True)
 
-intro = tk.Label(frame_menu, text='Jeu du Casse-Brique !', height=5)
-intro.pack()
-l1 = tk.Label(frame_menu, text='Nombre de vie (3 par défaut) :')
-l1.pack()
+tk.Label(frame_menu, text='Jeu du Casse-Brique !', height=5).pack()
+
+tk.Label(frame_menu, text='Nombre de vie (3 par défaut) :').pack()
 vies_entry = tk.Entry(frame_menu)
 vies_entry.pack()
-l2 = tk.Label(frame_menu, text='difficulté (1 à 3) :')
-l2.pack()
+
+tk.Label(frame_menu, text='difficulté (1 à 3) :').pack()
 diff_entry = tk.Entry(frame_menu)
 diff_entry.pack()
 
@@ -53,42 +52,47 @@ def entier(valeur):
     else:
         return False
 
+# Initialisation des variables globales
+canvas = None
+raquette = None
+balle = None
+blocs = None
+vies = 3 
+diff = 1 
+
+# Démarrage du jeu (à la pression du bouton Jouer)
 def initialisation():
+    # Paramètrages des variables globales
+    global canvas, raquette, balle, blocs, vies, diff
+    
+    if entier(vies_entry.get()):
+        vies = int(vies_entry.get())
+    
+    if diff_entry.get() in ['1', '2', '3']: 
+        diff = int(diff_entry.get())
+
+    # Création du canvas
     frame_menu.destroy()
+
+    frame_canvas = tk.Frame(window, width=1920, height=1060)
+    frame_canvas.pack(fill='both', expand=True)
+
+    canvas = tk.Canvas(frame_canvas, bg='light grey', width=1920, height=1060)
+    canvas.pack(fill='both')
+
+    canvas.create_line(0, 0, 1920, 0, fill='green', width=10)
+    canvas.create_line(0, 0, 0, 1060, fill='green', width=10)
+    canvas.create_line(1920, 0, 1920, 1060, fill='green', width=10)
+
+    # Création des objets 
+    raquette = Raquette.palet(canvas)
+    balle = Balle.balle(canvas)
+    blocs = Blocs.blocs(canvas, diff)
+    
     jeu()
-
-def setup():
-    return True
-
-vies = 1
-if entier(vies_entry.get()):
-    vies = int(vies_entry.get())
-
-diff = 1
-if diff_entry.get() != '' and diff_entry in '123':
-    vies = int(diff_entry.get())
 
 b1 = tk.Button(frame_menu, text='Jouer', command=initialisation)
 b1.pack()
-
-
-# Création du Canvas
-frame_canvas = tk.Frame(window, width=1920, height=1060)
-frame_canvas.pack(fill='both', expand=True)
-
-canvas = tk.Canvas(frame_canvas, bg='light grey', width=1920, height=1060)
-canvas.pack(fill='both')
-
-canvas.create_line(0, 0, 1920, 0, fill='green', width=10)
-canvas.create_line(0, 0, 0, 1060, fill='green', width=10)
-canvas.create_line(1920, 0, 1920, 1060, fill='green', width=10)
-
-# canvas.create_line(0, 1050, 1920, 1050, fill='red', width=10)
-
-# Création des objets 
-raquette = Raquette.palet(canvas)
-balle = Balle.balle(canvas)
-blocs = Blocs.blocs(canvas, diff)
 
 # Programme Principal
 
@@ -103,13 +107,14 @@ def jeu():
     global canvas, balle, blocs
     if blocs.vide():
         messagebox.showinfo(message='Bravo!')
+        #TODO
         return
 
     idbloc = balle.id_col(canvas)
     if idbloc == -1:
         balle.del_balle(canvas)
         window.destroy()
-        #TODO
+        #TODO La gestion des vies
     else:
         blocs.cassage(canvas, idbloc)
         balle.deplacement(canvas)
