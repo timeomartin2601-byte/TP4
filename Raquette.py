1579735a999d92f5fdd64cc42769c341880ddf1f
Arno Braz <110236956+ArnoBraz@users.noreply.github.com>
2025-10-23 11:57:01 +0200

    Update paddle movement logic and add stop functionality
    Refactor paddle movement and add stop method.

--- Raquette.py
+++ Raquette.py
@@ -12,26 +12,53 @@ class palet:
         palet : un rectangle (canvas) représentant la plateforme que le joueur controle
         '''
         self.__canvas = canvas
-        self.palet = self.__canvas.create_rectangle(x, y, x+dim[0], y+dim[1], fill="black")
-        self.mur_d=405
-        self.mur_g=295
+        self.__x, self.__y, self.__dim = x, y, dim
+        self.__paletg = self.__canvas.create_rectangle(x, y, x+(dim[0]/2), y+dim[1], fill="black")
+        self.__paletd = self.__canvas.create_rectangle(x+(dim[0]/2), y, x+dim[0], y+dim[1], fill="red")
+        self.__mur_d=405
+        self.__mur_g=295
+        self.__depl = 0
 
     def droite(self):
         '''
         Déplace la raquette à droite à chaque pression de la flèche droite
         '''
-        if self.mur_d <700:
-            self.__canvas.move(self.palet, 20, 0)
-            self.mur_d+=20
-            self.mur_g+=20            
+        if self.__mur_d+10 <700:
+            self.__depl = 10
+            self.__mur_d+=20
+            self.__mur_g+=20            
             # print(self.mur_d)
 
     def gauche(self):
         '''
         Déplace la raquette à gauche à chaque pression de la flèche gauche
         '''
-        if self.mur_g >0:
-            self.__canvas.move(self.palet, -20, 0)
-            self.mur_g-=20
-            self.mur_d-=20
+        if self.__mur_g-10 >0:
+            self.__depl = -10
+            self.__mur_g-=20
+            self.__mur_d-=20
             # print(self.mur_g)
+
+    def stop(self):
+        self.__depl = 0
+
+    def mouv(self):
+        '''
+        
+        '''
+        if self.__x+abs(self.__depl) < 700 or self.__x-abs(self.__depl)>0:
+            self.__x+=self.__depl
+        self.__canvas.coords(self.__paletg, self.__x, self.__y, self.__x+(self.__dim[0]/2), self.__y+self.__dim[1])
+        self.__canvas.coords(self.__paletd, self.__x+(self.__dim[0]/2), self.__y, self.__x+self.__dim[0], self.__y+self.__dim[1])
+
+    def id_paletg(self):
+        '''
+        
+        '''
+        return self.__paletg
+    
+    def id_paletd(self):
+        '''
+        
+        '''
+        return self.__paletd
